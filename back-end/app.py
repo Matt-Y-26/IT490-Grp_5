074be2329ebe5a2a9fde99762c2a268fd3b1eb8b
kaylac104 <70977160+kaylac104@users.noreply.github.com>
2020-10-11 20:59:05 -0400

    Update app.py

--- back-end/app.py
+++ back-end/app.py
@@ -4,10 +4,10 @@ import time
 time.sleep(20)
 
 print("Back end is running now")
-#credentials = pika.PlainCredentials('guest','guest')
-#connection = pika.BlockingConnection(
-#	pika.ConnectionParameters(
-#		host='messaging',
-#		credentials=credentials
-#	)
-#)
+credentials = pika.PlainCredentials('guest','guest')
+connection = pika.BlockingConnection(
+	pika.ConnectionParameters(
+		host='messaging',
+		credentials=credentials
+	)
+)
