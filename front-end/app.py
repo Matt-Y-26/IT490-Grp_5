628314f7b36c91ec2ef5bec7d8036d4d464b3ef2
kaylac104 <70977160+kaylac104@users.noreply.github.com>
2020-10-11 20:55:19 -0400

    Update app.py

--- front-end/app.py
+++ front-end/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Flask works in docker container!'
-
+    return 'Hey, we have Flask in a Docker container!'
+    
+@app.route('/login')
+def login():
+    return render_template('login.html');
+  
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
