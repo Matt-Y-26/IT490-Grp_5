624fdb8aada03990e072a7f3ccd2bb306cb392ea
Matt-Y <45631117+Matt-Y-26@users.noreply.github.com>
2020-10-12 02:57:41 -0400

    fixed app --added the register html

--- front-end/app.py
+++ front-end/app.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+	return 'Flask works in docker container!'
     
 @app.route('/login')
 def login():
-    return render_template('login.html');
-  
+	return render_template('login.html')
+    
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
