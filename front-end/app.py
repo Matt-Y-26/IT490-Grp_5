0c427cab49d30c4123ce6b0864f0b56cd0ddc3f8
Matt-Y <45631117+Matt-Y-26@users.noreply.github.com>
2020-10-19 02:06:10 -0400

    Files for midterm

--- front-end/app.py
+++ front-end/app.py
@@ -1,17 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, flash
+from werkzeug.security import check_password_hash, generate_password_hash
+import pika
+import messaging
+import os
+
 app = Flask(__name__)
+app.secret_key = os.environ['FLASK_SECRET_KEY']
+
 
 @app.route('/')
 def hello_world():
 	return 'Flask works in docker container!'
     
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-	return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        passwd = request.form['hashed']
+        mesg = messaging.Messaging()
+        mesg.send('GETHASH', {'username': username })
+        response = mesg.receive()
+        if response['success'] != True:
+            return "Login failed."
+        if check_password_hash(response['hash'], passwd):
+            session['username'] = username
+            return redirect('/')
+        else:
+            return "Failed, invalid."
+    return render_template('login.html')
     
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
-	return render_template('register.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        passwd = request.form['hashed']
+        #mseg stuff
+        mesg = messaging.Messaging()
+        mesg.send(
+            'REGISTER',
+            {
+                'username' : username,
+                'hashed': generate_password_hash(passwd)
+            }
+        )
+        response = mesg.receive()
+        if response['success']:
+                session['username'] = username
+                return redirect('/')
+        else:
+                return f"{response['message']}"
+    return render_template('register.html')
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
